e27f74479b8c9222afec595ab6becaa7d4222561
jasurbek602 <115354796+jasurbek602@users.noreply.github.com>
2025-05-07 14:59:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ bot.onText(/\/start/, (msg) => {
         ['Ro‘yxatdan o‘tish'],
         ['O‘qituvchilar haqida'],
         ['O‘quv markaz haqida'],
-        ['Admin bilan bog‘lanish']
+        ['Telegram kanal']
       ],
       resize_keyboard: true,
       one_time_keyboard: true,
@@ -33,31 +33,61 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  if (text === 'O‘qituvchilar haqida') {
-    bot.sendMessage(chatId, 'O‘qituvchilar:\n1.');
+  if (text == 'O‘qituvchilar haqida') {
+    bot.sendPhoto(chatId, 'AgACAgIAAxkBAAPtaBskqtLZ3nqozze9x4Gbq2AcY4IAAtTpMRtQTNlITF1tsxFx2xkBAAMCAAN5AAM2BA', { caption: 'Rajabaliyev Behruzbek \n Fan: Matematika \n Tajriba: 4 yil \n 1000tadan ortiq o`quvchiga talim bergan', 
+      reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "Ro‘yxatdan o‘tish", callback_data: 'register' }
+        ]
+      ]
+    }
+   });
+    bot.sendPhoto(chatId, 'AgACAgIAAxkBAAPvaBsk3xzUR1_Hy3hzPndmRCoxisIAAtXpMRtQTNlIMmtd3aYiJLEBAAMCAAN5AAM2BA', { caption: 'Ozodbek Qosimov \n Fan: Matematika Fizika \n Tajriba: 4 yil \n 1000taga yaqin o`quvchiga talim bergan',
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: "Ro‘yxatdan o‘tish", callback_data: 'register' }
+          ]
+        ]
+      }
+     });
   }
 
   if (text === 'O‘quv markaz haqida') {
     bot.sendMessage(chatId, 'Markazimiz Rapqon shaharcha, Abdurazoq MFY. Darslar yuqori sifatda olib boriladi.');
   }
 
-  if (text === 'Admin bilan bog‘lanish') {
+  if (text === 'Telegram kanal') {
     bot.sendMessage(chatId, 'Admin: @your_admin_username');
   }
 
   if (text === 'Ro‘yxatdan o‘tish') {
-    userSteps[chatId] = { step: 1 };
-    bot.sendMessage(chatId, "Iltimos, ism va familiyangizni kiriting:");
+    if (text == '/start') {
+      delete userSteps[chatId];
+    } else {
+      userSteps[chatId] = { step: 1 };
+      bot.sendMessage(chatId, "Iltimos, ism va familiyangizni kiriting:");
+    }
+    
   }
 
   const step = userSteps[chatId]?.step;
 
   if (step === 1 && text !== 'Ro‘yxatdan o‘tish') {
-    userSteps[chatId].name = text;
-    userSteps[chatId].step = 2;
-    bot.sendMessage(chatId, "Endi telefon raqamingizni kiriting:");
+    if (text == `/start`) {
+      delete userSteps[chatId];
+    } else{
+      userSteps[chatId].name = text;
+      userSteps[chatId].step = 2;
+      bot.sendMessage(chatId, "Endi telefon raqamingizni kiriting:");
+    }
+   
   } else if (step === 2) {
-    userSteps[chatId].phone = text;
+    if (text == `/start`) {
+      delete userSteps[chatId];
+    } else {
+      userSteps[chatId].phone = text;
     userSteps[chatId].step = 3;
 
     const subjects = {
@@ -69,10 +99,12 @@ bot.on('message', (msg) => {
     };
 
     bot.sendMessage(chatId, "Qaysi fan bo‘yicha ro‘yxatdan o‘tmoqchisiz?", subjects);
+    }
+    
   } else if (step === 3) {
     userSteps[chatId].subject = text;
 
-    const info = `Yangi ro‘yxatdan o‘tuvchi:\nIsm: ${userSteps[chatId].name}\nTelefon: ${userSteps[chatId].phone}\nFan: ${userSteps[chatId].subject}`;
+    const info = `Yangi ro‘yxatdan o‘tuvchi:\nIsm: ${userSteps[chatId].name}\nTelefon: ${userSteps[chatId].phone}\nFan: ${userSteps[chatId].subject}\nUsername: @${msg.chat.username}`;
 
     bot.sendMessage(adminChatId, info);
     bot.sendMessage(adminChat, info);
@@ -80,4 +112,15 @@ bot.on('message', (msg) => {
 
     delete userSteps[chatId];
   }
+});
+bot.on('callback_query', (query) => {
+  const chatId = query.message.chat.id;
+  const data = query.data;
+
+  if (data === 'register') {
+    userSteps[chatId] = { step: 1 };
+      bot.sendMessage(chatId, "Iltimos, ism va familiyangizni kiriting:");
+  }
+
+  bot.answerCallbackQuery(query.id); // Loading tugmasini to‘xtatish
 });
